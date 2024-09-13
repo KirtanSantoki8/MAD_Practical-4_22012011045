98be61b052b764110861e1ca7841808898baa29b
KirtanPatel8 <kirtansantoki12@gmail.com>
2024-09-13 10:41:21 +0530

    Initial Commit

---
+++ app/src/main/java/com/devkt/mad_practical_4_22012011045/AlarmBroadcastReceiver.kt
@@ -0,0 +1,13 @@
+package com.devkt.mad_practical_4_22012011045
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("AlarmBroadcastReceiver.onReceive() is not implemented")
+    }
+}
